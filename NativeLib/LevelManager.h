78108c1ed8a1b23066f5d79bd1ffcfc134df449f
Yur4ikMarchenko <marchenko_ak19@nuwm.edu.ua>
2021-05-12 15:01:12 +0300

    Switch lvls

--- NativeLib/LevelManager.h
+++ NativeLib/LevelManager.h
@@ -24,7 +24,7 @@ private:
     ResourceLoader* resourceLoader;
 
     String mainMenuPath = "res://Scenes/Menu.tscn";
-    String levelPath[5] = { "res://Scenes/World.tscn","res://Scenes/LVL2.tscn","res://Scenes/LVL3.tscn","res://Scenes/LVL4.tscn", "res://Scenes/LVL5.tscn" };
+    String levelPath[11] = { "res://Scenes/LVL1.tscn","res://Scenes/LVL2.tscn","res://Scenes/LVL3.tscn","res://Scenes/LVL4.tscn", "res://Scenes/LVL5.tscn","res://Scenes/LVL6.tscn","res://Scenes/LVL7.tscn","res://Scenes/LVL8a.tscn", "res://Scenes/LVL8b.tscn", "res://Scenes/LVL9.tscn" , "res://Scenes/LVL10.tscn" };
 
 public:
     Node* currentScene;
