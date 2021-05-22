3ca92075be330bc9e5d525f9daf15c753b334e53
Yur4ikMarchenko <marchenko_ak19@nuwm.edu.ua>
2021-05-22 13:27:23 +0300

    fix texture mistakes and added lvl 10

--- NativeLib/LevelManager.h
+++ NativeLib/LevelManager.h
@@ -25,7 +25,7 @@ private:
     ResourceLoader* resourceLoader;
 
     String mainMenuPath = "res://Scenes/Menu.tscn";
-    String levelPath[10] = { "res://Scenes/LVL1.tscn","res://Scenes/LVL2.tscn","res://Scenes/LVL3.tscn","res://Scenes/LVL4.tscn", "res://Scenes/LVL5.tscn","res://Scenes/LVL6.tscn","res://Scenes/LVL7.tscn","res://Scenes/LVL8a.tscn", "res://Scenes/LVL8b.tscn", "res://Scenes/LVL9.tscn"};
+    String levelPath[11] = { "res://Scenes/LVL1.tscn","res://Scenes/LVL2.tscn","res://Scenes/LVL3.tscn","res://Scenes/LVL4.tscn", "res://Scenes/LVL5.tscn","res://Scenes/LVL6.tscn","res://Scenes/LVL7.tscn","res://Scenes/LVL8a.tscn", "res://Scenes/LVL8b.tscn", "res://Scenes/LVL9.tscn","res://Scenes/LVL10.tscn"};
 
 public:
     Node* currentScene;
